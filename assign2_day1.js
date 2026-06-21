e9c7b26e23c2187e08084d908e42c2cbf0ebf4dd
Farheen Naaz <53914654+farheen2203@users.noreply.github.com>
2026-06-21 12:23:41 +0530

    Add files via upload
    week1_day1

---
+++ assign2_day1.js
@@ -0,0 +1,21 @@
+let genderType="female";
+function printGender()
+{
+    let color="brown";
+    if(genderType.startsWith("f"))
+        {
+        var age=30;
+        let color="pink";
+        console.log(color);
+        
+        }
+    console.log(age);
+    console.log(color);
+}
+printGender();
+console.log(genderType);
+genderType="male";
+printGender();
+console.log(genderType);
+
+
---
+++ assign_day1.js
@@ -0,0 +1,14 @@
+const browserName="Chrome";
+function getBrowserName() 
+{
+   
+    if(browserName=="Chrome")
+    {
+       var browserName;
+       //let browserName;
+    }
+   console.log("Browser name is: "+browserName);
+ 
+}
+getBrowserName();
+
